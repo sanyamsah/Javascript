5b431a4dd2c496d56b92e9c539398a05a6c13f91
sanyamsah <sanyamsah25@gmail.com>
2024-07-03 21:11:10 +0530

    new files

--- comparison.js
+++ comparison.js
@@ -8,3 +8,11 @@ console.log(null < 0); // false
 console.log(null == 0); // false
 console.log(null <= 0); // true
 console.log(null >= 0); // true
+// Equality check (==) and Comparisons (<,>,<=,>=) work differently
+// Comparisons convert null to a number
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false
+console.log(2 === 2); // true: checks datatype too
+console.log(100.0 == 100); // true
+console.log(100.0 === 100); // true
---
+++ datatypes.js
@@ -0,0 +1,27 @@
+// Primitive Types: Number, String, Boolean, null, undefined, Symbol, BigInt
+// Reference/Non-primitive Types: Array, Object, Functions
+// Javascript is a dynamically-typed language.
+
+const id = Symbol(123);
+const anotherId = Symbol(123);
+console.log(id == anotherId); // false
+
+const bigInt = 12345678901234567890n; // BigInt
+console.log(bigInt);
+
+const demigods = ["Indra", "Agni", "Vayu"]; // array
+console.log(demigods, typeof demigods); // type: object
+console.table(demigods);
+
+let obj ={
+    name : "Sanyam",
+    age : 22
+}
+console.log(obj);
+console.table(obj);
+
+const myFunction = function(){
+    console.log("Hello, World!");
+}
+myFunction(); // type: object function
+
