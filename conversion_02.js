c7c4da8143b7ffe0b7eb3399994f4453c7efd470
sanyamsah <sanyamsah25@gmail.com>
2024-07-03 17:44:41 +0530

    added new files

--- conversion_02.js
+++ conversion_02.js
@@ -1,3 +1,15 @@
 let value = 25;
 let negValue = -value;
-console.log(negValue, typeof negValue); 
+console.log(negValue, typeof negValue); // -25 number
+
+console.log(1 + 2); // 4
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(2 + 2 + "1"); // 41
+// ToPrimitive comes into play
+
+console.log(+true); // 1
+console.log(Number(true) + Number(true)); // 2
+console.log(+""); // 0
+
